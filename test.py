172072e2b5831a235a5d392a72d33203e458f636
Nanuka-1 <n.apriamova@example.com>
2025-12-12 16:55:03 +0400

    move test.py to secondbranch

--- test.py
+++ test.py
@@ -1,55 +1,22 @@
-import string
 
-#  მომხმარებლის შეყვანის შემოწმება
-user_input = input("შეიყვანე ფიბონაჩის რიცხვი: ")
+sentence = input("შემოიყვანე წინადადება: ").strip()
 
-#  ცარიელი ველი
-if user_input.strip() == "":
-    print("შენ არაფერი შეიყვანე! მხოლოდ რიცხვი უნდა შეიყვანო!")
-    exit()
+# სიტყვებად გაყოფა
+words = sentence.split()
 
-# თუ არის ასო
-if any(ch.isalpha() for ch in user_input):
-    print(f'შენ შეიყვანე ასო(ები): "{user_input}" — არასწორია! მხოლოდ რიცხვი შეიყვანე!')
-    exit()
+# ფუნქცია: წმენდა — ვაძლევთ მხოლოდ ასოებს
+def clean_word(word):
+    # შეგროვდება მხოლოდ ის სიმბოლოები, რომელთა isalpha() == True
+    return "".join(ch for ch in word if ch.isalpha())
 
-# თუ არის სიმბოლო
-if any(ch in string.punctuation for ch in user_input):
-    print(f'შენ შეიყვანე სიმბოლო(ები): "{user_input}" — არასწორია! მხოლოდ რიცხვი შეიყვანე!')
-    exit()
+# ქმნით შედეგის dictionary-ს:
+result = {}
+for w in words:
+    cw = clean_word(w)
+    if cw:
+        result[cw] = len(cw)
 
-# თუ არის space
-if any(ch.isspace() for ch in user_input):
-    print(f'შენ შეიყვანე დაშორება/space: "{user_input}" — არასწორია! მხოლოდ რიცხვი შეიყვანე!')
-    exit()
-
-# საბოლოო შემოწმება — არის თუ არა საერთოდ რიცხვი
-if not user_input.isdigit():
-    print(f'შენ შეიყვანე უცნაური ტექსტი: "{user_input}" — საჭიროა მხოლოდ მთელი რიცხვი!')
-    exit()
-
-
-
-n = int(user_input)
-
-# ფიბონაჩის დასაწყისი
-f1 = 1
-f2 = 1
-
-# თუ მომხმარებელმა მიუთითა 1
-if n == 1:
-    print(f1)
-    exit()
-
-# თუ მიუთითა 2
-print(f1, f2, end=" ")
-
-# while ციკლი
-while n > 2:
-    f1, f2 = f2, f1 + f2
-    print(f2, end=" ")
-    n -= 1
-
+print("\nResult:", result)
 
 
 
